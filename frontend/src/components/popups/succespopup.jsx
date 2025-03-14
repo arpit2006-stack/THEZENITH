de337409de7736831e749a749da78c4e199723aa
Samar-Qureshi <samarqureshi109@gmail.com>
2025-03-14 15:07:25 +0530

    final modal popup

--- frontend/src/components/popups/succespopup.jsx
+++ frontend/src/components/popups/succespopup.jsx
@@ -3,15 +3,15 @@ import { FaCheck } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 const succespopup = ({ onClose })=> {
   return (
-<div className="fixed inset-0 flex items-center justify-center  bg-opacity-30 backdrop-blur-sm">
-  <div className="bg-white rounded-2xl shadow-lg p-6 w-80 text-center flex flex-col items-center">
+<div className="fixed inset-0 flex items-center justify-center  bg-opacity-30 backdrop-blur-[3px]">
+  <div className="bg-white rounded shadow-lg p-6 w-80 text-center flex flex-col items-center">
     <div className="w-12 h-12 bg-green-500 flex items-center justify-center rounded-full text-white text-2xl mb-4">
       <FaCheck />
     </div>
-    <h2 className="text-xl font-bold text-gray-800">Welcome!</h2>
-    <p className="text-gray-600 text-sm mt-2">ZENITH STORE!</p>
-    <p className="text-gray-600 text-sm mt-2">You have logged in successfully!</p>
-    <button className="mt-4 bg-green-500 text-white text-sm py-2 px-6 rounded-lg hover:bg-green-600 flex items-center gap-2" onClick={onClose}>
+    <h2 className="text-4xl font-bold text-gray-800">Welcome!</h2>
+    <p className="text-gray-600 text-sm text-bold mt-4">ZENITH STORE</p>
+    <p className="text-gray-600 text-sm mt-4">You have logged in successfully!</p>
+    <button className="mt-6 bg-green-500 text-white text-sm py-2 px-6 rounded-lg hover:bg-green-600 flex items-center gap-2" onClick={onClose}>
       <FaArrowRight /> Admin Dashboard
     </button>
   </div>
--- frontend/src/pages/adminlogin.jsx
+++ frontend/src/pages/adminlogin.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "../lib/axios";
 import Succespopup from "../components/popups/succespopup";
 import background from "../assets/adminbg.jpg";
-
+import logo from "../assets/logo2.png";
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -38,7 +38,7 @@ const AdminLogin = () => {
         style={{ backgroundImage: `url(${background})` }}
       >
         <div className="ml-20 mt-10 p-6 rounded-lg shadow-2xl w-130">
-          <h2 className="text-4xl font-bold text-right mb-4">ZENITH STORE</h2>
+          <h2 className="text-4xl font-bold mb-4"><img src={logo} className="h-15 w-50 ml-65" alt="" /></h2>
           {error && <p className="text-red-500 text-center">{error}</p>}
           <form onSubmit={handleSubmit} className="space-y-4 text-xl">
             <div>
@@ -55,7 +55,7 @@ const AdminLogin = () => {
               <label className="block text-gray-700">Password</label>
               <input
                 type="password"
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-transparent"
+                className="w-full px-2 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-transparent"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
